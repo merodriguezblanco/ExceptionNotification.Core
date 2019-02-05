5961ef2f0445f82428da0a5f637c4b3f397cd033
Martin Rodriguez <merodriguezblanco@gmail.com>
2019-02-05 10:01:36 -0300

    Update initialization interface and bump version to v1.6.0

---
+++ ExceptionNotification.Core/ExceptionNotificationBuilderExtensions.cs
@@ -0,0 +1,24 @@
+﻿using ExceptionNotification.Core.Middlewares;
+using Microsoft.AspNetCore.Builder;
+
+namespace ExceptionNotification.Core
+{
+    public static class ExceptionNotificationBuilderExtensions
+    {
+        public static IApplicationBuilder AddExceptionNotification(this IApplicationBuilder app, IExceptionNotifierConfiguration configuration)
+        {
+            ExceptionNotifier.Setup(configuration);
+
+            app.UseMiddleware<ExceptionMiddleware>();
+
+            return app;
+        }
+
+        public static IApplicationBuilder AddExceptionNotification(this IApplicationBuilder app)
+        {
+            app.UseMiddleware<ExceptionMiddleware>();
+
+            return app;
+        }
+    }
+}
